1d742dd14108207d41f94263d4941e955a8e283c
root <root@vps202533.vps.ovh.ca>
2019-06-13 10:11:44 -0400

    add route config

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -1,8 +1,16 @@
 'use strict'
 module.exports = function(app){
-	var todoList = require('../controllers/todoListController');
+
+var todoList = require('../controllers/todoListController');
+
 app.route('/tasks')
-	.get(todoList.all_tasks)
+.get(todoList.all_task)
+
+
+app.route('/task') .post(todoList.create_task) ;
+app.route('/task/:id')
+.get(todoList.get_task)
+.delete(todoList.delete_task);
 
 
 
